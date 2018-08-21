6d3ef57ac0bddf7e7660595eab2273c0f727bd08
M. Lutfi Fauzi <mlfthedifferent@gmail.com>
2018-08-21 15:39:14 +0700

    Create Data Produk

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
+use App\Product;
+use File;
+use Image;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return view('products.index');
+        $products = Product::with('category')->orderBy('created_at', 'DESC')->paginate(10);
+        return view('products.index', compact('products'));
+    }
+
+    public function create()
+    {
+        $categories = Category::orderBy('name', 'ASC')->get();
+        return view('products.create', compact('categories'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'code' => 'required|string|max:10|unique:products',
+            'name' => 'required|string|max:100',
+            'description' => 'nullable|string|max:100',
+            'stock' => 'required|integer',
+            'price' => 'required|integer',
+            'category_id' => 'required|exists:categories,id',
+            'photo' => 'nullable|image|mimes:jpg,png,jpeg'
+        ]);
+
+        try {
+            $photo = null;
+            if ($request->hasFile('photo')) {
+                $photo = $this->saveFile($request->name, $request->file('photo'));
+            }
+
+            $product = Product::create([
+                'code' => $request->code,
+                'name' => $request->name,
+                'description' => $request->description,
+                'stock' => $request->stock,
+                'price' => $request->price,
+                'category_id' => $request->category_id,
+                'photo' => $photo
+            ]);
+            return redirect(route('produk.index'))
+                ->with(['success' => '<strong>' . $product->name . '</strong> Ditambahkan']);
+        } catch (\Exception $e) {
+            return redirect()->back()
+                ->with(['error' => $e->getMessage()]);
+        }
+    }
+
+    private function saveFile($name, $photo)
+    {
+        $images = str_slug($name) . time() . '.' . $photo->getClientOriginalExtension();
+        $path = public_path('uploads/product');
+
+        if (!File::isDirectory($path)) {
+            File::makeDirectory($path, 0777, true, true);
+        }
+        Image::make($photo)->save($path . '/' . $images);
+        return $images;
     }
 }
